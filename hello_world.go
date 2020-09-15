5e4a55dd4c359d1ee5af8e3258374b3bfc2a97c9
nikhil <nikhil@navigus.in>
2020-09-15 13:13:26 +0530

    spaces and print styles

--- hello_world.go
+++ hello_world.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import "fmt"	// formatting
 
 func main(){
-	fmt.Println("Hello world !")
+	fmt.Println("Hello world !")	// prints in new line
+	fmt.Print("Hey")
+	fmt.Print("you ! \n")
+
+	fmt.Printf("Hello %s Age is %d \n","Nikhil",24)		// Uses verbs to fill the dynamic values 
+
+	// %s string
+	// %d for integer 
+
 }
