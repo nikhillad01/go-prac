1367d6409d37020841d5cd29fd8f4a432eb69576
nikhil <nikhil@navigus.in>
2020-09-15 16:13:35 +0530

    basic shorthand techs

--- basic_syntax/basic.go
+++ basic_syntax/basic.go
@@ -1,9 +1,28 @@
 package main
 
-import "fmt"
+import ("fmt"
+		"reflect"
+	)
 
 func main(){
 	var name string = "Nikhil"
+	var name1,name2 = "first","name"
+	fmt.Println(name,name1,name2)
 
-	fmt.Println(name)
+
+	newVar := "variabl declared without declaring type or with keyword" //Dont use underscores in go variables use Camel-case
+
+	fmt.Println(newVar,reflect.TypeOf(newVar))
+
+	age := 20
+
+	if age < 20{
+		fmt.Println("You are minor")
+	} else if age > 20 {
+		fmt.Println("Greater than 20")
+	} else {
+		fmt.Println("Exat 20 ha")
+	}
+	
+
 }
