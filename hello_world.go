a80bdff6d9efcacb03d4053770f6cc8f9ee1af70
nikhil <nikhil@navigus.in>
2020-09-15 16:02:32 +0530

    basic variables

--- hello_world.go
+++ hello_world.go
@@ -1,6 +1,7 @@
 package main
 
-import "fmt"	// formatting
+import ("fmt"
+		"reflect")// formatting
 
 func main(){
 	fmt.Println("Hello world !")	// prints in new line
@@ -10,6 +11,24 @@ func main(){
 	fmt.Printf("Hello %s Age is %d \n","Nikhil",24)		// Uses verbs to fill the dynamic values 
 
 	// %s string
-	// %d for integer 
+	// %d for integer ch
+	// fmt.Println(true && true)
+	// fmt.Println(true && false)
+	// fmt.Println(false && false)
+
+	const a = 1
+	// var b int= 2
+	// var c string="333333"
+
+	fmt.Println(reflect.TypeOf(a))
+	// var c =23
+	// sum := 0
+	// for i:=0;i<10;i++{
+		
+	// 	sum+=i
+		
+	// }
+	// fmt.Println("--",sum)
+
 
 }
